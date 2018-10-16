853b0fc8a452c277d31f0044bb1ee0e27c999797
Robert Eric Maikher <robertem342@gmail.com>
2018-10-16 14:54:42 +0200

    Create task2 account

--- task2-project/src/main/scala/Account.scala
+++ task2-project/src/main/scala/Account.scala
@@ -14,7 +14,11 @@ class Account(val accountId: String, val bankId: String, val initialBalance: Dou
 
     private var transactions = HashMap[String, Transaction]()
 
-    class Balance(var amount: Double) {}
+    class Balance(var amount: Double) {
+        def reduce(number: Double): Unit = {amount -= number;}
+        def add(number: Double): Unit = {amount += number;}
+        def getAmount: Double = {amount;}
+    }
 
     val balance = new Balance(initialBalance)
 
@@ -23,22 +27,32 @@ class Account(val accountId: String, val bankId: String, val initialBalance: Dou
     }
 
     def getTransactions: List[Transaction] = {
-        // Should return a list of all Transaction-objects stored in transactions
-        ???
+        transactions.values.toList
     }
 
     def allTransactionsCompleted: Boolean = {
-        // Should return whether all Transaction-objects in transactions are completed
-        ???
+        getTransactions.forall(t => t.isCompleted)
     }
 
-    def withdraw(amount: Double): Unit = ??? // Like in part 2
-    def deposit(amount: Double): Unit = ??? // Like in part 2
-    def getBalanceAmount: Double = ??? // Like in part 2
+    def withdraw(amount: Double): Unit = this.synchronized {
+        if (amount < 0) throw new IllegalAmountException("Amount to withdraw must be positive")
+        if (amount > balance.getAmount) throw new NoSufficientFundsException("Cannot overdraw account. Balance must be greater than amount")
+        balance.reduce(amount)
+    }
+
+    def deposit(amount: Double): Unit = this.synchronized {
+        if (amount < 0) throw new IllegalAmountException("Amount to deposit must be positive")
+        balance.add(amount)
+    }
+
+    def getBalanceAmount: Double = {
+        balance.getAmount
+    }
 
     def sendTransactionToBank(t: Transaction): Unit = {
-        // Should send a message containing t to the bank of this account
-        ???
+        // Assume our own bank exists.
+        val bank: ActorRef = BankManager.findBank(bankId)
+        bank ! t
     }
 
     def transferTo(accountNumber: String, amount: Double): Transaction = {
@@ -69,21 +83,28 @@ class Account(val accountId: String, val bankId: String, val initialBalance: Dou
     }
 
     override def receive = {
-		case IdentifyActor => sender ! this
+        case IdentifyActor => sender ! this
 
-		case TransactionRequestReceipt(to, transactionId, transaction) => {
-			// Process receipt
-			???
-		}
+        case TransactionRequestReceipt(to, transactionId, transaction) =>
+            // Assume transactionId and transaction is valid
+            transactions(transactionId).status = transaction.status
+            transactions(transactionId).receiptReceived = true
 
-		case BalanceRequest => ??? // Should return current balance
+        case BalanceRequest => sender ! getBalanceAmount
 
-		case t: Transaction => {
-			// Handle incoming transaction
-			???
-		}
+        case t: Transaction =>
+            try {
+                deposit(t.amount)
+                t.status = TransactionStatus.SUCCESS
+            } catch {
+                case _: NoSufficientFundsException | _: IllegalAmountException =>
+                    t.status = TransactionStatus.FAILED
+            }
+            sender ! TransactionRequestReceipt(toAccountNumber = t.from, transactionId = t.id,
+                transaction = t)
 
-		case msg => ???
+
+        case msg => ???
     }
 
 
